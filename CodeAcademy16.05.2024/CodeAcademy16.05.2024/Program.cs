135a647060c62e66475c41f30f9c537ec3a91016
Ilaha1996 <ilahaah@code.edu.az>
2024-05-16 12:37:07 +0400

    tasks added

---
+++ CodeAcademy16.05.2024/CodeAcademy16.05.2024/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+
+namespace CodeAcademy16._05._2024
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] chars = { 'c', 'o', 'd', 'e', 'a', 'c', 'a', 'd', 'e', 'm', 'y' };
+            char targetChart = chars[0]; // c
+            int countChart = 0;            
+           
+            for (int i = 0; i < chars.Length; i++) 
+            {
+                if (chars[i] == targetChart) 
+                
+                {                  
+                    countChart++;
+                    
+                }
+            }
+            Console.WriteLine($"Number of '{targetChart}' in the array: {countChart}");
+        }
+            
+    }
+}
---
+++ OddNumbers16.05.2024/OddNumbers16.05.2024/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.Design;
+
+namespace OddNumbers16._05._2024
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int start = 15;
+            int end = 80;
+
+            if (end > start)
+            { 
+                               
+              
+                if (start % 2 == 1 && end % 2 == 1)
+                {
+                    Console.WriteLine((end - start) / 2 -1 );   
+                }
+
+                else
+                {
+                Console.WriteLine((end - start) / 2);
+                }
+
+
+            }
+        }
+    }
+}
